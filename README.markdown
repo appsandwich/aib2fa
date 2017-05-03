37c1be5b9f3528f9a16854bb3928ea7c2908e1ca
vinny.coyne <vinny.coyne@j2global.com>
2017-05-03 13:11:44 +0100

    initial commit

---
+++ README.markdown
@@ -0,0 +1,28 @@
+# AIB to FreeAgent Conversion
+
+Since AIB now allow small businesses to use the Personal Internet Banking system, instead of the archaic IBB system, it's become a lot easier to export transactions as CSV.
+
+This script will convert the AIB CSV into a format that can be uploaded to [FreeAgent](https://www.freeagent.com).
+
+It's built on top of the awesome [Marathon](https://github.com/JohnSundell/Marathon) Swift scripting system.
+
+## Installation
+
+### Install Marathon
+
+Visit the [Marathon Github page](https://github.com/JohnSundell/Marathon) for installation instructions.
+ 
+### Installing the Script
+
+`git clone ...`
+
+`cd aib2fa`
+
+`marathon install aib2fa`
+
+ 
+### Usage
+
+`aib2fa /path/to/your/file.csv`
+
+This will generate a new file at `/path/to/your/file-converted.csv`
---
+++ aib2fa.swift
@@ -0,0 +1,159 @@
+import Foundation;
+
+class Transaction {
+    
+    var explanation: String
+    var value: String
+    var date: String
+    
+    init(exp: String, value: String, date: String) {
+        self.explanation = exp
+        self.value = value
+        self.date = date
+    }
+    
+    func debugPrint() -> String {
+        return "Date: \(self.date)\nExplanation: \(self.explanation)\nValue: \(self.value)\n\n"
+    }
+    
+    func exportDate() -> String {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yy"
+        
+        let timestamp = dateFormatter.date(from: self.date)
+        
+        guard let ts = timestamp else {
+            return ""
+        }
+        
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        
+        return dateFormatter.string(from: ts)
+    }
+    
+    func exportFormat() -> String {
+        return self.exportDate() + "," + self.value + ",'\(self.explanation)'"
+    }
+}
+
+func formatLine(_ line:String) -> Transaction? {
+    
+    let columns = line.components(separatedBy: ",")
+    
+    guard columns.count == 7 else {
+        return nil
+    }
+    
+    let date = columns[1]
+    
+    let explanation = columns[2]
+    
+    let debit = columns[3]
+    let credit = columns[4]
+    
+    if debit.characters.count > 0 || credit.characters.count > 0 {
+        
+        let value = debit.characters.count > 0 ? "-" + debit : credit
+        
+        let transaction = Transaction(exp: explanation, value: value, date: date)
+        
+        //print("\(transaction.exportFormat())")
+        
+        return transaction
+    }
+    else {
+        return nil
+    }
+}
+
+func outputFileURLForInputFileURL(_ inputFileURL: URL) -> URL {
+    
+    let withoutExtension = inputFileURL.deletingPathExtension()
+    
+    return URL(fileURLWithPath: withoutExtension.path + "-converted.csv")
+}
+
+func writeTransactions(_ transactions: [Transaction], toFileURL: URL) {
+    
+    print("ℹ️  Writing \(transactions.count) transactions...")
+    
+    let outputStrings: [String] = transactions.map { (t) -> String in
+        return t.exportFormat()
+    }
+    
+    let outputString = outputStrings.joined(separator: "\n")
+    
+    //print("\(outputString)")
+    
+    do {
+        try outputString.write(toFile: toFileURL.path, atomically: true, encoding: String.Encoding.utf8)
+    }
+    catch {
+        print("🛑  Write failed!")
+        exit(7)
+    }
+    
+    print("✅  Exported to \(toFileURL).")
+}
+
+
+let arguments = CommandLine.arguments
+
+guard arguments.count > 1 else {
+    print("⁉️  No filename given.")
+    exit(1)
+}
+
+let file = arguments[1]
+
+let fileManager = FileManager.default
+
+guard fileManager.fileExists(atPath: file) else {
+    print("🛑  File does not exist.")
+    exit(2)
+}
+
+let fileURL = URL(fileURLWithPath: file)
+
+var fileString: String? = nil
+
+do {
+    fileString = try String(contentsOf: fileURL, encoding: String.Encoding.utf8)
+}
+catch {
+    print("🛑  Could not read file.")
+    exit(3)
+}
+
+guard let fs = fileString else {
+    print("🛑  File is empty.")
+    exit(4)
+}
+
+var lines = fs.components(separatedBy: .newlines)
+
+guard lines.count > 0 else {
+    print("🛑  File is empty.")
+    exit(5)
+}
+
+
+var transactions: [Transaction] = []
+
+lines.remove(at: 0)
+
+lines.forEach { (line) in
+    
+    if let transaction = formatLine(line) {
+        transactions.append(transaction)
+    }
+}
+
+guard transactions.count > 0 else {
+    print("🛑  No transactions found.")
+    exit(6)
+}
+
+writeTransactions(transactions, toFileURL: outputFileURLForInputFileURL(fileURL))
+
